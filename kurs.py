b4019bed89ce299335b45d79bf93902c806268dd
Munevera Plojovic <93732677+muneveraplojovic@users.noreply.github.com>
2022-05-29 10:54:37 +0200

    1 homework

---
+++ kurs.py
@@ -0,0 +1,26 @@
+# Tipovi podataka: prossti i slozeni 
+# varijable sa celim brojem - int - intiger
+# brojevi sa decimalama - float 
+# string - niz karaktera
+# print - ispis necega u terminalu
+
+# brojgodina = 17
+# moj_broj = 5.78
+# print(brojgodina)
+
+num1=int(input("Unesite broj: "))
+num2=int(input("Unesite broj: "))
+num3=int(input("Unesite broj: "))
+
+sum=num1+num2+num3
+div=int(sum/3)
+print("Aritmeticka sredina vasih unetih brojeva je: ", div)
+
+
+a=int(input("Unesite duzinu stranice a: "))
+b=int(input("Unesite duzinu stranice b: "))
+
+povrsina=int(a*b)
+print("Povrsina vaseg pravougaonika je: ", povrsina)
+
+
