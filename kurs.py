9d12511479f0e5504f5806be18e5346619d52dba
Munevera Plojovic <93732677+muneveraplojovic@users.noreply.github.com>
2022-05-30 23:51:42 +0200

    2 homework

--- kurs.py
+++ kurs.py
@@ -3,24 +3,67 @@
 # brojevi sa decimalama - float 
 # string - niz karaktera
 # print - ispis necega u terminalu
+# bool - true/false
+# pretvaranje iz jednog tipa u drugi
+# type
+
 
 # brojgodina = 17
 # moj_broj = 5.78
 # print(brojgodina)
 
-num1=int(input("Unesite broj: "))
-num2=int(input("Unesite broj: "))
-num3=int(input("Unesite broj: "))
+# num1=int(input("Unesite broj: "))
+# num2=int(input("Unesite broj: "))
+# num3=int(input("Unesite broj: "))
 
-sum=num1+num2+num3
-div=int(sum/3)
-print("Aritmeticka sredina vasih unetih brojeva je: ", div)
-
-
-a=int(input("Unesite duzinu stranice a: "))
-b=int(input("Unesite duzinu stranice b: "))
-
-povrsina=int(a*b)
-print("Povrsina vaseg pravougaonika je: ", povrsina)
+# sum=num1+num2+num3
+# div=int(sum/3)
+# print("Aritmeticka sredina vasih unetih brojeva je: ", div)
 
 
+# a=int(input("Unesite duzinu stranice a: "))
+# b=int(input("Unesite duzinu stranice b: "))
+
+# povrsina=int(a*b)
+# print("Povrsina vaseg pravougaonika je: ", povrsina)
+
+
+# x,y,z="banana", "cherry", "chocolate"
+# print(x,z,y)
+
+# x=z=y="Bananana"
+# print(x)
+# print(z)
+# print(y)
+
+# Operatori
+# -aritmeticki operatori (+-*/%**)
+#- relacijski-rezultat je uvek bool- (<><=>=, ==, != )
+# - operatori za dodelu vrednosti - += -=
+
+#Logicki operator - true/false
+# and-true if both of them are true
+# or- true if either one of them are true
+# not - not true-false, not false-true
+
+
+# obim i povrsina kruga
+# pi=3.14
+# r=int(input("Unesite broj poluprecnika: "))
+
+# pov=2*r*pi
+# obim=2**2*pi
+# print("povrsina", pov)
+# print("obim", obim)
+#----------------------------------------------
+
+# unesite 3 stranice trougla i da se izracuna obim
+
+
+# str1=int(input("unesite stranicu 1: "))
+# str2=int(input("unesite stranicu 2: "))
+# str3=int(input("unesite stranicu 3: "))
+
+# obim=str1+str2+str3
+
+# print("obim je: ", obim)
