4602c441cab8b3768263c378c5782159bce49e84
Munevera Plojovic <93732677+muneveraplojovic@users.noreply.github.com>
2022-06-05 12:41:18 +0200

    neww commit

--- kurs.py
+++ kurs.py
@@ -1,9 +1,10 @@
-# Tipovi podataka: prossti i slozeni 
+# Tipovi podataka: prosti i slozeni 
 # varijable sa celim brojem - int - intiger
 # brojevi sa decimalama - float 
-# string - niz karaktera
-# print - ispis necega u terminalu
 # bool - true/false
+# string - niz karaktera
+
+# print - ispis necega u terminalu
 # pretvaranje iz jednog tipa u drugi
 # type
 
@@ -42,9 +43,9 @@
 # - operatori za dodelu vrednosti - += -=
 
 #Logicki operator - true/false
-# and-true if both of them are true
-# or- true if either one of them are true
-# not - not true-false, not false-true
+# and-true if both of them are true, za povezivanje izraza
+# or- true if either one of them are true, 
+# not - not true-false, not false-true, 
 
 
 # obim i povrsina kruga
@@ -69,7 +70,59 @@
 # print("obim je: ", obim)
 
 # galon=4.54
-# litri=int(input("Unesite broj litara: "))
+# litri=float(input("Unesite broj litara: "))
 
 # sum=litri*galon
 # print(sum)
+
+# ------------------------------------------------------------------------------
+
+# x=int(input("unesite broj: "))
+# y=int(input("unesite broj: "))
+
+# z=(x**2+y**2)**2
+# print("vrednost izraza je: ", z)
+
+# galoni=float(input("Unesite galone: "))
+# litri= galoni* 4.54
+# print(litri)
+
+
+# broj1= float(input("Unesite broj 1: "))
+# broj2= float(input("Unesite broj 2: "))
+# broj3= float(input("Unesite broj 3: "))
+
+# # rezultat=(broj1+broj2)/broj3
+
+# print((broj1+broj2)/broj3)
+# skola="gimnazija"
+# print(skola[2 : ])
+# # prva vrednost se racuna a zadnja se ne racuna
+
+# print(skola[2: :])
+# # pocetna, zavrsna, korak 
+
+# print(skola[2: :2])
+# # print(skola[ : :-1]) ispise rec gimnazija unazad
+
+
+
+# Metode stringa:
+# -indeksiranje 0123456789
+# -indeksiranje sa minusom -1 -2 -3 -4 -5 -6 -7 -8 -9 (zadnji broj ima indeks -1)
+# -replace
+# skola= "gimnazija"
+# rezultat= skola.replace("g", "G")
+# print(rezultat)
+# # -length len - vraca druzinu stringa, ne funkcionise kod brojeva
+# -upper
+
+# rec= str(input("unesite rec: "))
+# rez= rec.replace(rec[0], rec[0].upper())
+# print(rez)
+
+
+
+
+
+# ----------------------------------------------------------------------------------
