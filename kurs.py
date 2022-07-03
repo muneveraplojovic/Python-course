fa60aa4165aa3e3e68b482d40bfa1728bf2e1090
Munevera Plojovic <93732677+muneveraplojovic@users.noreply.github.com>
2022-07-03 02:34:15 +0200

    new commit

--- kurs.py
+++ kurs.py
@@ -496,8 +496,16 @@
 
 # projcentureler
 
+# for if else elif operatori and or not [::]
 
 
+# for a in range(1,1000):
+#     for b  in range(1,1000):
+#         for c in range(1,1000):
+#             if a<b<c and a**2+b**2==c**2 and a+b+c==1000:
+#                 print(a*b*c)
+#                 break
+# --------------------------------------------------------------------------------------------
 
 
 
