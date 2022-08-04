2bc7a4695ace5d6cf64e886ba59e32a5891b207d
Munevera Plojovic <plojovicm3@gmail.com>
2022-08-04 21:46:31 +0200

    new commit

--- kurs.py
+++ kurs.py
@@ -499,13 +499,14 @@
 # for if else elif operatori and or not [::]
 
 
-for a in range(1,1000):
-    for b  in range(1,1000):
-        for c in range(1,1000):
-            if a<b<c and a**2+b**2==c**2 and a+b+c==1000:
-                print(a*b*c)
-                break
+# for a in range(1,1000):
+#     for b  in range(1,1000):
+#         for c in range(1,1000):
+#             if a<b<c and a**2+b**2==c**2 and a+b+c==1000:
+#                 print(a*b*c)
+#                 break
 # --------------------------------------------------------------------------------------------
 
+# liste, matrice, append, remove, sort, extend, nizovi, pop, insert, clear:
 
-
+#             
